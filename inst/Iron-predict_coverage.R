bb89c32e6e6f13beb1e36b8d390bcc095fd3a723
Naoto-Imamachi <imamachigatugatu@hotmail.co.jp>
2016-03-01 20:53:00 +0900

    no message

--- inst/Iron-predict_coverage.R
+++ inst/Iron-predict_coverage.R
@@ -1,65 +1,107 @@
+## Alpine function: predict.R <https://github.com/mikelove/alpine>
+
+#gene <- ebt2[[test_ELAVL1_RXID]]
+#genome <- Hsapiens
+#readlength <- 48
+#minsize <- 100
+#maxsize <- 300
+
 predictOneGene <- function(gene, bamfile, fitpar, genome=Hsapiens,
                            models, readlength, minsize, maxsize) {
     #Checking
     stopifnot(is(gene, "GRanges"))
     stopifnot(all(sapply(models, function(x) names(x) %in% c("formula","offset"))))
     stopifnot(!is.null(fitpar))
-
+    
+    #bamfile <- list.files(bamfile)
+    
+    #Prepare dummy reads(All possible fragment patterns)
     fragtypes <- buildFragtypesFromExons(gene, genome, readlength=readlength,
                                          minsize=minsize, maxsize=maxsize)
+    
+    #Initialize result list
     res <- list()
-    bamname <- bamfile
-    # add counts
+    
+    #The range of test_gene (GRanges object)
     generange <- range(gene)
     strand(generange) <- "*" # not necessary
+    
+    #Mapped reads on each transcript (st -> ed (including exon/intron))
     suppressWarnings({
         ga <- readGAlignAlpine(bamfile, generange)
     })
+    
+    #IF: mapped reads -> 0
     if (length(ga) == 0) {
-        res[[bamname]] <- as.list(rep(NA,length(models)))
-        names(res[[bamname]]) <- names(models)
+        res[["test"]] <- as.list(rep(NA,length(models)))
+        names(res[["test"]]) <- names(models)
         next
     }
+    
     ga <- keepSeqlevels(ga, as.character(seqnames(gene)[1]))
+    
+    #Compatible reads on each transcript (st -> ed (including only exon!!))
     fco <- findCompatibleOverlaps(ga, GRangesList(gene))
-    # message("-- ",round(length(fco)/length(ga),2)," compatible overlaps")
+    
+    #Transcript position of compatible reads
     reads <- gaToReadsOnTx(ga, GRangesList(gene), fco)
     
     # save fragment coverage for later
-    l <- sum(width(gene))
+    l <- sum(width(gene)) #Transcript length
+    
+    #Remove the following reads: Start/End site == 1
     frag.cov <- coverage(reads[[1]][start(reads[[1]]) != 1 & end(reads[[1]]) != l])
     
+    #Add count data to fragtypes(dummy data)
     fragtypes.temp <- matchReadsToFraglist(reads, list(fragtypes))[[1]]
+    
     ## -- fragment bias --
-    fraglen.density <- fitpar[[bamname]][["fraglen.density"]]
-    stopifnot(!is.null(fraglen.density))
+    fraglen.density <- fitpar[[1]][["fraglen.density"]]
+    stopifnot(!is.null(fraglen.density)) #Checking
+    
+    #Calculate the density of fragment length from mapped reads
     fragtypes.temp$logdfraglen <- log(matchToDensity(fragtypes.temp$fraglen, fraglen.density))
+    
     ## -- random hexamer priming bias with VLMM --
-    vlmm.fivep <- fitpar[[bamname]][["vlmm.fivep"]]
-    vlmm.threep <- fitpar[[bamname]][["vlmm.threep"]]
+    vlmm.fivep <- fitpar[[1]][["vlmm.fivep"]] #5'side
+    vlmm.threep <- fitpar[[1]][["vlmm.threep"]] #3'side
     stopifnot(!is.null(vlmm.fivep))
     stopifnot(!is.null(vlmm.threep))
+    
+    #Calculate log(bias) for each fragment based on the VLMM
     fragtypes.temp <- addVLMMBias(fragtypes.temp, vlmm.fivep, vlmm.threep)
     
     # -- fit models --
-    res[[bamname]] <- list()
+    res[[1]] <- list()
     
     # remove first and last bp for predicting coverage along transcript
     not.first.or.last.bp <- !(fragtypes.temp$start == 1 | fragtypes.temp$end == l)
     fragtypes.temp <- fragtypes.temp[not.first.or.last.bp,]
     
+    #Prepare the range/width of fragment read
     ir <- IRanges(fragtypes.temp$start, fragtypes.temp$end)
-    res[[bamname]]$l <- l
-    res[[bamname]]$frag.cov <- frag.cov
-    res[[bamname]]$pred.cov <- list()
+    
+    #Output result
+    res[[1]]$l <- l #Transcript length
+    res[[1]]$frag.cov <- frag.cov #raw coverage
+    res[[1]]$pred.cov <- list()
+    
     for (modeltype in names(models)) {
         # message("predicting model type: ",modeltype)
-        log.lambda <- getLogLambda(fragtypes.temp, models, modeltype, fitpar, bamname)
+        log.lambda <- getLogLambda(fragtypes.temp, models, modeltype, fitpar, bamfile)
         pred0 <- exp(log.lambda)
         pred <- pred0/mean(pred0)*mean(fragtypes.temp$count)
-        res[[bamname]][["pred.cov"]][[modeltype]] <- coverage(ir, weight=pred)
+        #res[[1]][["pred.cov"]][[modeltype]] <- coverage(ir, weight=pred)
+        
+        fragtypes.temp.test <- fragtypes.temp
+        fragtypes.temp.test$pred <- pred
+        fragtypes.temp.test$ir <- ir
+        fragtypes.temp.test2 <- fragtypes.temp.test[fragtypes.temp.test$count > 0,]
+        pred.mapped <- fragtypes.temp.test2$pred*100
+        ir.mapped <- fragtypes.temp.test2$ir
+        res[[1]][["pred.cov"]][["GC"]] <- coverage(ir.mapped, weight=pred.mapped)
     }
-res    
+    return(res)
 }
 
 
--- inst/Iron_run.R
+++ inst/Iron_run.R
@@ -53,7 +53,7 @@ txdf <- AnnotationDbi::select(txdb,
                               keys = trxids, 
                               keytype = "TXNAME") #WARNING: which select function ?
 txdf.txid <- txdf$TXID
-txdf.txid <- txdf$TXID[1:20] #TEST: selected 10 transcripts with single-3UTR
+txdf.txid <- txdf$TXID[1:40] #TEST: selected 10 transcripts with single-3UTR
 
 #Extract exon information from gtf file
 ebt <- exonsBy(txdb, "tx")
@@ -72,6 +72,7 @@ source("C:/Users/Naoto/Documents/Visual Studio 2015/Projects/EndClip/EndClip/ins
 source("C:/Users/Naoto/Documents/Visual Studio 2015/Projects/EndClip/EndClip/inst/Iron-data_prep_func.R")
 source("C:/Users/Naoto/Documents/Visual Studio 2015/Projects/EndClip/EndClip/inst/Iron-VLMM.R")
 source("C:/Users/Naoto/Documents/Visual Studio 2015/Projects/EndClip/EndClip/inst/Iron-GLM.R")
+source("C:/Users/Naoto/Documents/Visual Studio 2015/Projects/EndClip/EndClip/inst/Iron-predict_coverage.R")
 
 #Prepare two demensional matrix stored the information about the aligned paired-end fragments
 #fragtypes <- lapply(genenames, function(gene) {
@@ -107,105 +108,14 @@ models <- list("null" = list(formula = NULL, offset = NULL),
                "GC" = list(formula = "count ~ ns(gc, knots = gc.knots, Boundary.knots = gc.bk) + 0",
                offset = c("fraglen", "vlmm")))
 
-#TEST: 
+#TEST: ELAVL1
 test_ELAVL1_RXID <- txdf2[txdf2$GENEID == "ELAVL1",]$TXID
-ebt2[[test_ELAVL1_RXID]]
+#ebt2[[test_ELAVL1_RXID]]
 
-test_res <- predictOneGene(ebt2[[test_ELAVL1_RXID]], bamfile, fitpar, genome=Hsapiens,
-                           models, readlength = 48, minsize = 100, maxsize = 300)
-
-gene <- ebt2[[test_ELAVL1_RXID]]
-genome <- Hsapiens
-readlength <- 48
-minsize <- 100
-maxsize <- 300
+res <- predictOneGene(ebt2[[test_ELAVL1_RXID]], bamfile, fitpar, genome=Hsapiens,
+                      models, readlength = 48, minsize = 100, maxsize = 300)
 
 #Checking
-stopifnot(is(gene, "GRanges"))
-stopifnot(all(sapply(models, function(x) names(x) %in% c("formula","offset"))))
-stopifnot(!is.null(fitpar))
-
-bamfile <- list.files(bamfile)
-
-#Prepare dummy reads(All possible fragment patterns)
-fragtypes <- buildFragtypesFromExons(gene, genome, readlength=readlength,
-                                     minsize=minsize, maxsize=maxsize)
-
-#Initialize result list
-res <- list()
-
-#The range of test_gene (GRanges object)
-generange <- range(gene)
-strand(generange) <- "*" # not necessary
-
-#Mapped reads on each transcript (st -> ed (including exon/intron))
-suppressWarnings({
-    ga <- readGAlignAlpine(bamfile, generange)
-})
-
-#IF: mapped reads -> 0
-if (length(ga) == 0) {
-    res[["test"]] <- as.list(rep(NA,length(models)))
-    names(res[["test"]]) <- names(models)
-    next
-}
-
-ga <- keepSeqlevels(ga, as.character(seqnames(gene)[1]))
-
-#Compatible reads on each transcript (st -> ed (including only exon!!))
-fco <- findCompatibleOverlaps(ga, GRangesList(gene))
-
-#Transcript position of compatible reads
-reads <- gaToReadsOnTx(ga, GRangesList(gene), fco)
-
-# save fragment coverage for later
-l <- sum(width(gene)) #Transcript length
-
-#Remove the following reads: Start/End site == 1
-frag.cov <- coverage(reads[[1]][start(reads[[1]]) != 1 & end(reads[[1]]) != l])
-
-#Add count data to fragtypes(dummy data)
-fragtypes.temp <- matchReadsToFraglist(reads, list(fragtypes))[[1]]
-
-## -- fragment bias --
-fraglen.density <- fitpar[[1]][["fraglen.density"]]
-stopifnot(!is.null(fraglen.density)) #Checking
-
-#Calculate the density of fragment length from mapped reads
-fragtypes.temp$logdfraglen <- log(matchToDensity(fragtypes.temp$fraglen, fraglen.density))
-
-## -- random hexamer priming bias with VLMM --
-vlmm.fivep <- fitpar[[1]][["vlmm.fivep"]] #5'side
-vlmm.threep <- fitpar[[1]][["vlmm.threep"]] #3'side
-stopifnot(!is.null(vlmm.fivep))
-stopifnot(!is.null(vlmm.threep))
-
-#Calculate log(bias) for each fragment based on the VLMM
-fragtypes.temp <- addVLMMBias(fragtypes.temp, vlmm.fivep, vlmm.threep)
-
-# -- fit models --
-res[[1]] <- list()
-
-# remove first and last bp for predicting coverage along transcript
-not.first.or.last.bp <- !(fragtypes.temp$start == 1 | fragtypes.temp$end == l)
-fragtypes.temp <- fragtypes.temp[not.first.or.last.bp,]
-
-#Prepare the range/width of fragment read
-ir <- IRanges(fragtypes.temp$start, fragtypes.temp$end)
-
-#Output result
-res[[1]]$l <- l #Transcript length
-res[[1]]$frag.cov <- frag.cov #raw coverage
-res[[1]]$pred.cov <- list()
-
-for (modeltype in names(models)) {
-    # message("predicting model type: ",modeltype)
-    log.lambda <- getLogLambda(fragtypes.temp, models, modeltype, fitpar, bamfile)
-    pred0 <- exp(log.lambda)
-    pred <- pred0/mean(pred0)*mean(fragtypes.temp$count)
-    res[[1]][["pred.cov"]][[modeltype]] <- coverage(ir, weight=pred)
-}
-
 plotCov <- function(res, m="GC", cond, xlab="", ylab="", log=FALSE, lwd=3, ...) {
     for (i in seq_along(res)) {
         transform <- if (log) {
