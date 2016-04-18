c8d6635490d98ff1b498c48b235380ba38f15773
Naoto-Imamachi <imamachigatugatu@hotmail.co.jp>
2016-04-18 14:36:46 +0900

    Add new script for preparing map file

--- EndClip_preparation.sh
+++ EndClip_preparation.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-python3 EndClip_prep.py data/EndClip_prep_configure_file.txt
+echo "-- Prepare map file from annotation gtf file..."
+python3 A_make_map_from_gtf.py /mnt/hgfs/Projects/EndClip/EndClip/data/gencode.v24.basic.annotation.gtf /mnt/hgfs/Projects/EndClip/EndClip/data/gencode.v24.basic.annotation.map
+
+echo "-- Prepare 3UTR database file from annotation gtf file..."
+#python3 EndClip_prep.py data/EndClip_prep_configure_file.txt
